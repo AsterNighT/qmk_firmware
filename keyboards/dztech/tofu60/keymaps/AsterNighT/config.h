da55fa17242222e5e6beffad241cc3a0429cfee0
AsterNighT <asternight@outlook.com>
2024-07-06 17:38:22 +0800

    Update
    Signed-off-by: AsterNighT <asternight@outlook.com>

---
+++ keyboards/dztech/tofu60/keymaps/AsterNighT/config.h
@@ -0,0 +1,28 @@
+/* Copyright 2024 AsterNighT <asternight@outlook.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// tapping hold
+#define TAPPING_TERM 300
+#define HOLD_ON_OTHER_KEY_PRESS
+
+// caps word
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+
+// auto shift
+#define AUTO_SHIFT_TIMEOUT 150
+
+// dynamic macro
+#define DYNAMIC_MACRO_NO_NESTING
---
+++ keyboards/dztech/tofu60/keymaps/AsterNighT/rules.mk
@@ -0,0 +1,6 @@
+LEADER_ENABLE = yes # https://docs.qmk.fm/features/leader_key
+AUTO_SHIFT_ENABLE = yes # https://docs.qmk.fm/features/auto_shift
+DYNAMIC_TAPPING_TERM_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+# TAP_DANCE_ENABLE = yes # https://docs.qmk.fm/features/tap_dance
